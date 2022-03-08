f599e407493f50fee8d57e2fc59458a01abdebbd
disooloo <disooloo@mail.ru>
2022-03-08 13:57:23 +0500

    init laravel + vue

---
+++ app/resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<!-- Scripts -->
+	<script src="{{ asset('js/app.js') }}" defer></script>
+
+	<!-- Fonts -->
+	<link rel="dns-prefetch" href="//fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+	200
+	<hr>
+	<div id="app">
+		<example-component></example-component>
+	</div>
+</body>
+
+</html>
